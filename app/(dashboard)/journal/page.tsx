24d8b497118de8760c31a1bef34b40ea0025dd57
PrashanthBalakrishnan <prashbala13@gmail.com>
2023-06-26 19:07:03 -0400

    question feature was removed from UI

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -27,9 +27,9 @@ const JournalPage = async () => {
     return (
         <div className="p-10 bg-zinc-400/30 h-full">
             <h2 className="text-3xl mb-8 ">Journals</h2>
-            <div className="my-8">
+            {/* <div className="my-8">
                 <Question />
-            </div>
+            </div> */}
             <div className="grid grid-cols-3 gap-4">
                 <NewEntryCard />
                 {entries.map((entry) => (
